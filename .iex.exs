0bb48563e82c9a862aeaba66f643df4e1aca48ec
Christopher Eyre <chriseyre2000@gmail.com>
2021-06-19 10:35:24 +0100

    Added Enum,each demo

---
+++ .iex.exs
@@ -0,0 +1,6 @@
+emails = [
+  "hello@world.com",
+  "hola@world.com",
+  "nihao@world.com",
+  "konnichiwah@world.com"
+]
--- lib/sender.ex
+++ lib/sender.ex
@@ -1,7 +1,11 @@
 defmodule Sender do
-  def send_email(_email) do
+  def send_email(email) do
     Process.sleep(3_000)
-    IO.puts("Email to #{Email} sent")
+    IO.puts("Email to #{email} sent")
     {:ok, "email_sent"}
+  end
+
+  def notify_all(emails) do
+    Enum.each(emails, &send_email/1)
   end
 end
