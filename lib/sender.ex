fda1f0c6bb591eee13a332196a95fed2e5573bb6
Christopher Eyre <chriseyre2000@gmail.com>
2021-06-19 11:26:04 +0100

    Added and used a supervisor

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,4 +1,8 @@
 defmodule Sender do
+  def send_email("konnichiwah@world.com" = email) do
+    raise "Oops could not send an email to #{email}"
+  end
+
   def send_email(email) do
     Process.sleep(3_000)
     IO.puts("Email to #{email} sent")
@@ -6,8 +10,8 @@ defmodule Sender do
   end
 
   def notify_all(emails) do
-    emails
-    |> Task.async_stream(&send_email/1, max_concurrency: 2, ordered: false, on_timeout: :kill_task)
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
     |> Enum.to_list()
   end
 end
