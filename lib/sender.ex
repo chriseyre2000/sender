6130e6e36422e18ce03fd339839fc0b9dcc056b0
Christopher Eyre <chriseyre2000@gmail.com>
2021-06-19 10:46:41 +0100

    Using async_stream with max_concurrency

--- lib/sender.ex
+++ lib/sender.ex
@@ -7,7 +7,7 @@ defmodule Sender do
 
   def notify_all(emails) do
     emails
-    |> Task.async_stream(&send_email/1)
+    |> Task.async_stream(&send_email/1, max_concurrency: 2)
     |> Enum.to_list()
   end
 end
