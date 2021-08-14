d2168297f1dd05c56fad23fe32f9cace4450c632
Christopher Eyre <chriseyre2000@gmail.com>
2021-08-14 12:44:37 +0100

    Initial version

---
+++ lib/sender.ex
@@ -0,0 +1,15 @@
+defmodule Sender do
+  @moduledoc """
+  Documentation for `Sender`.
+  """
+
+  def send_email(email) do
+    Process.sleep(3_000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
+  end
+
+  def notify_all(emails) do
+    Enum.each(emails, &send_email/1)
+  end
+end
